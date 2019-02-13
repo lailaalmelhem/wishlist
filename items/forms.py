daf8d08cc0a6c051807b803dd80e55936bd19c98
Laila Al Melhem <lailaalmelhem@Lailas-MacBook-Pro.local>
2019-02-13 16:33:56 +0300

    submission

--- items/forms.py
+++ items/forms.py
@@ -2,14 +2,21 @@ from django import forms
 from django.contrib.auth.models import User
 
 class UserRegisterForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = [ 'username', 'first_name', 'last_name', 'password']
+	class Meta:
+		model = User
+		fields = [ 'username', 'first_name', 'last_name', 'password']
+		widgets = {
+			'password': forms.PasswordInput(),
+		}
+
 
 class UserLoginForm(forms.Form):
-    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Username'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
-    fields = ['username', 'password']
+	username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Username'}))
+	password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
+	fields = ['username', 'password']
+	widgets = {
+        	'password': forms.PasswordInput(),
+        }
 
 
 
